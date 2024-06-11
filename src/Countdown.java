9c18e6e31ecc71a30498a1d83463c2161360e003
ALUNO <ALUNO@10.85.198.12>
2024-06-11 16:43:21 -0300

    Commit project Countdown

---
+++ src/Countdown.java
@@ -0,0 +1,13 @@
+public class Countdown {
+
+    public static void main(String[] args) {
+        
+        System.out.println("contagem regressiva para lancamento: ");
+
+        for(int i = 100; i >= 0; i--) {
+            System.out.print(i +" "); 
+        }
+
+        System.out.println("Blast Off!");
+    }
+}
